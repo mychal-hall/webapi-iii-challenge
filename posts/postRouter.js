0e56e047e2124daa18f59f1b13edc248bec70ac4
Mychal Hall <hallmychal@gmail.com>
2019-05-14 21:03:48 -0500

    posts get up

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,8 +1,22 @@
 const express = require("express");
 
+const Posts = require("./postDb.js");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+// Can use this because the server.js is defining the default route that grabs the posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Posts.get(req.query);
+    res.status(200).json(posts);
+  } catch (error) {
+    //log error to server
+    console.log(error);
+    res.status(500).json({
+      message: "Error receiving the posts."
+    });
+  }
+});
 
 router.get("/:id", (req, res) => {});
 
