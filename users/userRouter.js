85345500c338b326016060120b72451197e52ce2
Mychal Hall <hallmychal@gmail.com>
2019-05-14 21:35:28 -0500

    Get Users is up

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,43 @@
-const express = require('express');
+const express = require("express");
+
+const Users = require("./userDb.js");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+// Can use this because the server.js is defining this default route that grabs the users
 
+// /api/users
+router.get("/", async (req, res) => {
+    try {
+        const users = await Users.get(req.body);
+        res.status(200).json(users);
+      } catch (error) {
+        // log error to server
+        console.log(error);
+        res.status(500).json({ message: "Error retreiving the Users" });
+      }
 });
 
-router.post('/:id/posts', (req, res) => {
-
+// /api/users
+router.post("/", async (req, res) => {
 });
 
-router.get('/', (req, res) => {
+router.post("/:id/posts", (req, res) => {});
 
-});
+router.get("/:id", (req, res) => {});
 
-router.get('/:id', (req, res) => {
+router.get("/:id/posts", (req, res) => {});
 
-});
+router.delete("/:id", (req, res) => {});
 
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
