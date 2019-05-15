6b3c7276b23344adc6ccb812e16a0d4f2ddb2975
Mychal Hall <hallmychal@gmail.com>
2019-05-14 21:55:16 -0500

    built 3rd middleware

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const server = express();
 
+// defining the routers
 const postRouter = require("./posts/postRouter.js");
 const userRouter = require("./users/userRouter.js");
 
 server.use(express.json());
+server.use(logger);
 
+// Setting up the router default endpoints
 server.use("/api/posts/", postRouter);
 server.use("/api/users/", userRouter);
 
@@ -16,6 +19,10 @@ server.get("/", (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const time = new Date();
+  console.log(`${req.method} Request`, time, `${req.url}`);
+  next();
+}
 
 module.exports = server;
