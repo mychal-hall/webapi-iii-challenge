07e97be269503538fc3daf7d2be66fd5f2321c67
Mychal Hall <hallmychal@gmail.com>
2019-05-14 20:56:33 -0500

    server up

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = 'express';
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+const postRouter = require("./posts/postRouter.js");
+const userRouter = require("./users/userRouter.js");
+
+server.use(express.json());
+
+server.use("/api/posts/", postRouter);
+server.use("/api/users/", userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
