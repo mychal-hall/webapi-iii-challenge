dff5bd8b5c5ed8f52ce4821dae1fca55bf4bd402
Mychal Hall <hallmychal@gmail.com>
2019-05-14 21:19:49 -0500

    Delet post is up

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -27,9 +27,22 @@ router.get("/:id", validatePostId, async (req, res) => {
 
 // /api/posts/:id
 router.delete("/:id", validatePostId, async (req, res) => {
-    try {
-        const count = await Posts.
+  try {
+    const count = await Posts.remove(req.params.id);
+    if (count > 0) {
+      res
+        .status(200)
+        .json({
+          message: "The post is gone, but the internet will always remember"
+        });
+    } else {
+      res.status(404).json({ message: "The post could not be found" });
     }
+  } catch (error) {
+    //log error to server
+    console.log(error);
+    res.status(500).json({ message: "Error removing the post." });
+  }
 });
 
 router.put("/:id", (req, res) => {});
