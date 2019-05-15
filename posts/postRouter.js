6767b922ff27096aebe353c98d4f418b1c77fec7
Mychal Hall <hallmychal@gmail.com>
2019-05-14 21:13:11 -0500

    Post getByID is up/ 1st middleware built

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -5,6 +5,8 @@ const Posts = require("./postDb.js");
 const router = express.Router();
 
 // Can use this because the server.js is defining the default route that grabs the posts
+
+// /api/posts
 router.get("/", async (req, res) => {
   try {
     const posts = await Posts.get(req.query);
@@ -18,14 +20,35 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {});
+// /api/posts/:id
+router.get("/:id", validatePostId, async (req, res) => {
+  res.status(200).json(req.post);
+});
 
-router.delete("/:id", (req, res) => {});
+// /api/posts/:id
+router.delete("/:id", validatePostId, async (req, res) => {
+    try {
+        const count = await Posts.
+    }
+});
 
 router.put("/:id", (req, res) => {});
 
 // custom middleware
 
-function validatePostId(req, res, next) {}
+async function validatePostId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const post = await Posts.getById(id);
+    if (post) {
+      req.post = post;
+      next();
+    } else {
+      res.status(404).json({ message: "Error -- Post not found!" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Error -- Erasing Hard Drive" });
+  }
+}
 
 module.exports = router;
